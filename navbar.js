f14c71d572fa99feaddddc84529260c5fd5fd702
a6265305 <a6265305@yahoo.com.tw>
2015-11-13 21:48:08 +0800

    index.html

---
+++ navbar.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+                $(".toggle").click(function() {
+                    $(this).toggleClass("active");
+                    $(".nav").slideToggle();
+                });
+                $(".nav > ul > li:has(ul) > a").append('<div class="arrow-bottom"></div>');
+            });
